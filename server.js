b97ddeef201f1d8abba72b597739ae879b5eda71
Shimmyshanga <luca.junghans@gmail.com>
2017-09-02 16:48:07 -0700

    api security cookies

--- server.js
+++ server.js
@@ -4,22 +4,28 @@ const http       = require('http'),
       session    = require('express-session'),
 	path       = require('path'),
       httpErrors = require('httperrors');
+      helmet     = require('helmet');
+      MongoStore = require('connect-mongo')(session);
 	PORT_NUM   = '3000';
       app        = express();
 
 // Get our API routes -- This connects into back end Logic // 
 require('./api/config/mongoose.js');
 const api = require('./api/config/routes.js');
+const mongoose = require('mongoose');
 
  //Express App Build
 app.use(session({
 	secret:'secretLuca',
 	resave: false,
-	saveUninitialized : true,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection })
 }));
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(helmet());
 app.use(bodyParser.json());
 app.use('/api', api);
+// UNCOMMENT FOR PRODUCTION!!!!!!
+app.use(express.static(path.join(__dirname, 'dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
@@ -28,5 +34,5 @@ app.set('port', port);
 
 const server = http.createServer(app);
 
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+server.listen(port, () => console.log(` API running on localhost: ${ port } `));
 
